3c338468d480bccd56295cc87f216275b7007836
Abhay <kabhay849@gmail.com>
2026-06-22 23:27:45 +0530

    added few changes

--- src/main/java/com/airbnb/user/service/UserService.java
+++ src/main/java/com/airbnb/user/service/UserService.java
@@ -1,8 +1,5 @@
 package com.airbnb.user.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
+public interface UserService {
 
 }
---
+++ src/main/java/com/airbnb/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.airbnb.user.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.airbnb.user.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
